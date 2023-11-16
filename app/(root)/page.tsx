468252f20ba0bd99339745d29932823ddd228b27
JoshMusira <joshuamusira01@gmail.com>
2023-11-16 20:03:08 +0300

    feat: configure shadcn ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <p>Pipe</p>
+            <Button className="P-5">Click me</Button>
+        </main>
+    )
+}
