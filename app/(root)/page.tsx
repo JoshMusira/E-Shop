0ed3d2cf68e09c6a849cdd30d63bc089cb5898c7
JoshMusira <joshuamusira01@gmail.com>
2023-11-16 23:55:27 +0300

    feat:finish store category

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+'use client'
+// import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+// import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
     return (
         <main className="p-6">
-            <UserButton afterSignOutUrl='/' />
-            <Button className="P-5">Click me</Button>
+            {/* <UserButton afterSignOutUrl='/' /> */}
+            <Modal title='Eshop' description='Testing the store' isOpen onClose={() => { }}>
+                children
+            </Modal>
         </main>
     )
 }
