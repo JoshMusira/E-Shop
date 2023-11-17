c4c565bfb62159473d4e08d31d1e4943a71c649f
JoshMusira <joshuamusira01@gmail.com>
2023-11-17 17:33:28 +0300

    feat: create a store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,24 @@
 'use client'
-// import { Button } from "@/components/ui/button";
-import { Modal } from "@/components/ui/modal";
-// import { UserButton } from "@clerk/nextjs";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+import { useEffect } from "react";
+
 
 export default function Home() {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen])
+
     return (
-        <main className="p-6">
-            {/* <UserButton afterSignOutUrl='/' /> */}
-            <Modal title='E-shop' description='Testing the store' isOpen onClose={() => { }}>
-                children
-            </Modal>
-        </main>
+        <div className="p-6">
+            Root Page
+
+        </div>
     )
 }
