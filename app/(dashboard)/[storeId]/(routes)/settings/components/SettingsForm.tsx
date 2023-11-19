ae5fa9a2ed21771a0f37bc929c25e10927e8af6d
JoshMusira <joshuamusira01@gmail.com>
2023-11-19 21:08:38 +0300

    feat: create settings page

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,8 +1,98 @@
 
+"use client"
 
-const SettingsForm = () => {
+import * as z from "zod"
+import axios from "axios"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "react-hot-toast"
+import { Trash } from "lucide-react"
+import { Store } from "@prisma/client"
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Separator } from "@/components/ui/separator"
+import { Heading } from "@/components/ui/Heading"
+// import { AlertModal } from "@/components/modals/alert-modal"
+// import { ApiAlert } from "@/components/ui/api-alert"
+// import { useOrigin } from "@/hooks/use-origin"
+
+const formSchema = z.object({
+    name: z.string().min(2),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>
+
+interface SettingsFormProps {
+    initialData: Store;
+};
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({
+    initialData
+}) => {
+    const params = useParams();
+    const router = useRouter();
+    // const origin = useOrigin();
+
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    });
+
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data)
+    }
+
     return (
-        <div>SettingsForm</div>
+        <>
+            <div className="flex items-center justify-between">
+                <Heading title="Store settings" description="Manage store preferences" />
+                <Button
+                    disabled={loading}
+                    variant="destructive"
+                    size="sm"
+                    onClick={() => setOpen(true)}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+            <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input disabled={loading} placeholder="Store name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button disabled={loading} className="ml-auto" type="submit">
+                        Save changes
+                    </Button>
+                </form>
+            </Form>
+        </>
     )
 }
 
